ca67e48a46226ba4a71212dc97c2400edc16578c
katumwa Lauren <95852478+katumwalauren@users.noreply.github.com>
2024-06-17 00:38:06 +0000

    1st get

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Lauren this is codespace")
+
+const app = require('./app')
+
+const {PORT} = express.env
+
+app.listen(PORT, () => {
+    console.log(`server is running at port:${PORT}`)
+})
