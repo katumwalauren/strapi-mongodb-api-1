db93c05d75ab05e18b5cddbc1f27a6e445aeeeb8
katumwa Lauren <95852478+katumwalauren@users.noreply.github.com>
2024-06-17 22:37:37 +0000

    updated mongodb

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+require("./database/database").connect()
+
 const express = require('express')
 
 const app = express()
--- database/database.js
+++ database/database.js
@@ -1,14 +1,23 @@
-const mongoose = require("mongoose")
-const MONGODB_URL = process.env
-exports.connect  = () => {
-mongoose.connect(MONGODB_URL, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}, )
-.then()
-.catch(() => {
-    console.log("DB connection FAILED")
-    console.log(error)
-    process.exit(1)
-})
-}
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+
+const  {MONGODB_URL}  = process.env;
+
+exports.connect = () => {
+    console.log(typeof MONGODB_URL);
+
+    mongoose.connect(MONGODB_URL, {
+        //useNewUrlParser: true,
+        //useUnifiedTopology: true,
+    })
+        .then(() => {
+            console.log(`DB connection SUCCESS`);
+        })
+        .catch((error) => {
+            console.log(`DB connection FAILED`);
+            console.log(error);
+            process.exit(1);
+        });
+};
+
